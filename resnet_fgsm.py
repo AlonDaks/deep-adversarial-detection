1ec780ad1502115be8ef46215b83a7bc1f8d6e9a
nishdesai <nish.sdesai@gmail.com>
2016-10-25 01:40:44 -0700

    various code changes

--- resnet_fgsm.py
+++ resnet_fgsm.py
@@ -1,4 +1,5 @@
 import keras
+import keras.backend as K
 import numpy as np
 from keras.preprocessing import image as image_utils
 
@@ -11,6 +12,7 @@ from cleverhans.utils_tf import tf_model_train, tf_model_eval, batch_eval
 from cleverhans.attacks import fgsm
 
 from keras.applications.resnet50 import ResNet50
+from keras.utils import np_utils
 
 FLAGS = flags.FLAGS
 
@@ -19,7 +21,7 @@ flags.DEFINE_string('filename', 'mnist.ckpt', 'Filename to save model under.')
 flags.DEFINE_integer('nb_classes', 1000, 'Number of classification classes')
 flags.DEFINE_integer('img_rows', 224, 'Input row dimension')
 flags.DEFINE_integer('img_cols', 224, 'Input column dimension')
-
+flags.DEFINE_integer('batch_size', 128, 'Size of training batches')
 
 def preprocess_input(x, dim_ordering='default'):
     if dim_ordering == 'default':
@@ -42,27 +44,33 @@ def preprocess_input(x, dim_ordering='default'):
 
 
 def data_resnet():
-    file_names = ['ILSVRC2012_val_00000001.JPEG',
-        'ILSVRC2012_val_00000002.JPEG',
-        'ILSVRC2012_val_00000003.JPEG',
-        'ILSVRC2012_val_00000004.JPEG',
-        'ILSVRC2012_val_00000005.JPEG',
-        'ILSVRC2012_val_00000006.JPEG',
-        'ILSVRC2012_val_00000007.JPEG',
-        'ILSVRC2012_val_00000008.JPEG',
-        'ILSVRC2012_val_00000009.JPEG',
-        'ILSVRC2012_val_00000010.JPEG']
-    labels = [490, 361, 171, 822, 297, 482, 13, 704, 599, 164]
+    file_names = ['ILSVRC2012_val_00000{0:03d}.JPEG'.format(i+1) for i in range(100)]
+        # 'ILSVRC2012_val_00000002.JPEG',
+        # 'ILSVRC2012_val_00000003.JPEG',
+        # 'ILSVRC2012_val_00000004.JPEG',
+        # 'ILSVRC2012_val_00000005.JPEG',
+        # 'ILSVRC2012_val_00000006.JPEG',
+        # 'ILSVRC2012_val_00000007.JPEG',
+        # 'ILSVRC2012_val_00000008.JPEG',
+        # 'ILSVRC2012_val_00000009.JPEG',
+        # 'ILSVRC2012_val_00000010.JPEG']
+    labels = [490,361,171,822,297,482,13,704,599,164,649,11,73,286,554,6,648,399,749,545,13,204,318,693,399,304,102,207,480,780,644,275,14,954,249,790,501,547,809,606,297,927,424,156,60,983,256,207,281,456,413,498,561,750,182,267,118,893,597,840,836,107,647,471,945,451,214,790,291,837,707,193,397,568,401,705,200,202,31,949,361,98,709,483,563,695,122,497,914,476,102,199,104,221,138,257,188,436,229,52]
 
-    images = np.array()
-    for f in file_names:
-        image = image_utils.load_img('./ILSVRC2012_img_val/{0}'.format(f), target_size=(224, 224))
+    images = np.empty((10, 3, FLAGS.img_rows, FLAGS.img_cols))
+    for i in range(10):
+        image = image_utils.load_img('./ILSVRC2012_img_val/{0}'.format(file_names[i]), target_size=(224, 224))
         image = image_utils.img_to_array(image)
         image = np.expand_dims(image, axis=0)
         image = preprocess_input(image)
-        images = np.append(images, image, axis=0)
+        # image = image.transpose((0,3,1,2))
+        images[i,:,:,:] = image
     print images.shape
 
+    images = images.astype('float32')
+
+    X_train, Y_train = images[:5,:,:,:], labels[:5]
+    X_test, Y_test = images[5:,:,:,:], labels[5:]
+    
 
     # X_train = X_train.astype('float32')
     # X_test = X_test.astype('float32')
@@ -73,9 +81,9 @@ def data_resnet():
     # print(X_test.shape[0], 'test samples')
 
     # # convert class vectors to binary class matrices
-    # Y_train = np_utils.to_categorical(y_train, FLAGS.nb_classes)
-    # Y_test = np_utils.to_categorical(y_test, FLAGS.nb_classes)
-    # return X_train, Y_train, X_test, Y_test
+    Y_train = np_utils.to_categorical(Y_train, FLAGS.nb_classes)
+    Y_test = np_utils.to_categorical(Y_test, FLAGS.nb_classes)
+    return X_train, Y_train, X_test, Y_test
 
 def main(argv=None):
     """
@@ -94,7 +102,7 @@ def main(argv=None):
 
     # Get MNIST test data
     X_train, Y_train, X_test, Y_test = data_resnet()
-    print "Loaded MNIST test data."
+    print "Loaded ImageNet test data."
 
     # Define input TF placeholder
     x = tf.placeholder(tf.float32, shape=(None, 3, 224, 224))
@@ -102,47 +110,24 @@ def main(argv=None):
 
     # Define TF model graph
     model = ResNet50(input_tensor=x)
-    predictions = model(x)
+    predictions = model.outputs[0]
     print "Defined TensorFlow model graph."
 
     # Evaluate the accuracy of the MNIST model on legitimate test examples
     accuracy = tf_model_eval(sess, x, y, predictions, X_test, Y_test)
-    assert X_test.shape[0] == 10000, X_test.shape
+    # assert X_test.shape[0] == 10000, X_test.shape
     print 'Test accuracy on legitimate test examples: ' + str(accuracy)
 
     # Craft adversarial examples using Fast Gradient Sign Method (FGSM)
     adv_x = fgsm(x, predictions, eps=0.3)
     X_test_adv, = batch_eval(sess, [x], [adv_x], [X_test])
-    assert X_test_adv.shape[0] == 10000, X_test_adv.shape
+    # assert X_test_adv.shape[0] == 10000, X_test_adv.shape
 
     # Evaluate the accuracy of the MNIST model on adversarial examples
     accuracy = tf_model_eval(sess, x, y, predictions, X_test_adv, Y_test)
     print'Test accuracy on adversarial examples: ' + str(accuracy)
 
-    print "Repeating the process, using adversarial training"
-    # Redefine TF model graph
-    model_2 = model_mnist()
-    predictions_2 = model_2(x)
-    adv_x_2 = fgsm(x, predictions_2, eps=0.3)
-    predictions_2_adv = model_2(adv_x_2)
 
-    # Perform adversarial training
-    tf_model_train(sess, x, y, predictions_2, X_train, Y_train, predictions_adv=predictions_2_adv)
-
-    # Evaluate the accuracy of the adversarialy trained MNIST model on
-    # legitimate test examples
-    accuracy = tf_model_eval(sess, x, y, predictions_2, X_test, Y_test)
-    print 'Test accuracy on legitimate test examples: ' + str(accuracy)
-
-    # Craft adversarial examples using Fast Gradient Sign Method (FGSM) on
-    # the new model, which was trained using adversarial training
-    X_test_adv_2, = batch_eval(sess, [x], [adv_x_2], [X_test])
-    assert X_test_adv_2.shape[0] == 10000, X_test_adv_2.shape
-
-    # Evaluate the accuracy of the adversarially trained MNIST model on
-    # adversarial examples
-    accuracy_adv = tf_model_eval(sess, x, y, predictions_2, X_test_adv_2, Y_test)
-    print'Test accuracy on adversarial examples: ' + str(accuracy_adv)
-
-# if __name__ == '__main__':
-#     app.run()
+if __name__ == '__main__':
+    app.run()
+    # data_resnet()
