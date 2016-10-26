a370b9fc19617539e8c063c8ac2ea9c430c820c5
nishdesai <nish.sdesai@gmail.com>
2016-10-26 12:34:39 -0700

    downgrade setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 sudo apt-get install imagemagick
 
-wget https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda-repo-ubuntu1404-8-0-local_8.0.44-1_amd64-deb
-sudo dpkg -i cuda-repo-ubuntu1404-8-0-local_8.0.44-1_amd64.deb
+wget http://developer.download.nvidia.com/compute/cuda/7.5/Prod/local_installers/cuda-repo-ubuntu1404-7-5-local_7.5-18_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1404-7-5-local_7.5-18_amd64.deb
 sudo apt-get update
 sudo apt-get install cuda
-rm cuda-repo-ubuntu1404-8-0-local_8.0.44-1_amd64.deb
+rm cuda-repo-ubuntu1404-7-5-local_7.5-18_amd64.deb
 
 
 wget https://bootstrap.pypa.io/get-pip.py
@@ -12,7 +12,7 @@ sudo python get-pip.py
 rm get-pip.py
 
 #Install cuDNN
-tar -xvf cudnn-8.0-linux-x64-v5.1.tgz -C ~
+tar -xvf cudnn-7.5-linux-x64-v5.1.tgz -C ~
 pushd ~
 sudo cp cuda/include/cudnn.h /usr/local/cuda/include
 sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
@@ -21,7 +21,7 @@ popd
 
 sudo pip install -r requirements.txt
 
-export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.11.0rc1-cp27-none-linux_x86_64.whl
+export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.9.0-cp27-none-linux_x86_64.whl
 sudo pip install --upgrade $TF_BINARY_URL
 
 pushd ~
