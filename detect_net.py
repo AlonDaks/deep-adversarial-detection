a1258f895e2768cac4de507fa098c76d90e57f78
Alon Daks <alon.daks@berkeley.edu>
2016-12-11 13:40:52 -0800

    reduced alexnet to 1 conv layers

--- detect_net.py
+++ detect_net.py
@@ -119,10 +119,10 @@ def alex_detect_net(mode=2):
     model.add(Activation('relu'))
     model.add(MaxPooling2D((3, 3), strides=(2, 2)))
 
-    model.add(Convolution2D(256, 5, 5, name='conv2'))
-    # model.add(BatchNormalization(axis=bn_axis, name='bn_conv2', mode=mode))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D((3, 3), strides=(2, 2)))
+    # model.add(Convolution2D(256, 5, 5, name='conv2'))
+    # # model.add(BatchNormalization(axis=bn_axis, name='bn_conv2', mode=mode))
+    # model.add(Activation('relu'))
+    # model.add(MaxPooling2D((3, 3), strides=(2, 2)))
 
     # model.add(Convolution2D(384, 3, 3, name='conv3'))
     # # model.add(BatchNormalization(axis=bn_axis, name='bn_conv3', mode=mode))
